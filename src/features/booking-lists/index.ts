b4f2d72bb680cc2a254c59fce970c9ccca05b538
kimura0603 <k.kimura0603@gmail.com>
2023-06-10 09:58:53 +0900

    モジュール設計：featureフォルダ

---
+++ src/features/booking-lists/index.ts
@@ -0,0 +1,12 @@
+// components:
+export * from './components/component-a;
+//export * from './components/component-b';
+//export * from './components/component-c';
+//export * from './components/component-d';
+
+// api:
+export * from './api/api-a';
+// export * from './api/api-b';
+
+// types:
+//export * from './types';
---
+++ src/features/recipes/components/component-a/component-a.tsx
@@ -0,0 +1,9 @@
+//import {} from ~
+//import { XXX } from '../../types';
+
+//基本的にAPIは使わない
+//appの中のコンポーネントでたたいたデータをこのコンポーネントに渡す
+
+export const ComponentA = () => {
+  return <div>ComponentA</div>;
+}
