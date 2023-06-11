ed8e263247414a5f4fd7db4fcfbfe043073fbdb2
takeyu1013 <yuto1013@gmail.com>
2023-06-11 22:33:46 +0900

    ⏪

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import { use } from "react";
-
-import { api } from "@/server/trpc/server";
-
 export default function Home() {
-  const { greeting } = use(api.sayHello.query({ name: "client" }));
-
-  return (
-    <main className="text-red-500">
-      <div>
-        <p>{greeting}</p>
-      </div>
-    </main>
-  );
+  return <main className="text-red-500">page</main>;
 }
