a7fac9ce222ca4ca97131caeea42ba0788d10eb2
kimura0603 <k.kimura0603@gmail.com>
2023-06-14 00:34:14 +0900

    kyselyのdockerのmysql接続設定

--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -1,12 +1,27 @@
-import { Kysely } from "kysely";
+import { createPool } from 'mysql2'
+import { Kysely, MysqlDialect } from "kysely";
 import { PlanetScaleDialect } from "kysely-planetscale";
 import { fetch } from "undici";
-
 import { DB } from "@/types/types";
 
-export const db = new Kysely<DB>({
-  dialect: new PlanetScaleDialect({
+let dialect = null;
+if (process.env.KYSELEY_DB_DIALECT === 'mysql') {
+  dialect = new MysqlDialect({
+    pool: createPool({
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    })
+  });
+}
+if (process.env.KYSELEY_DB_DIALECT === 'planetscale') {
+  dialect = new PlanetScaleDialect({
     url: process.env.DATABASE_URL,
     fetch,
-  }),
+  });
+}
+
+export const db = new Kysely<DB>({
+  dialect
 });
