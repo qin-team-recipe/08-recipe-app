7a8d00900789b5b7490f3e34c35b3c178f983948
takeyu1013 <yuto1013@gmail.com>
2023-06-14 22:01:25 +0900

    🗃️

--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -1,27 +1,26 @@
-import { createPool } from 'mysql2'
+import { ConnectionString } from "connection-string";
 import { Kysely, MysqlDialect } from "kysely";
 import { PlanetScaleDialect } from "kysely-planetscale";
+import { createPool } from "mysql2";
 import { fetch } from "undici";
+
 import { DB } from "@/types/types";
 
-let dialect = null;
-if (process.env.KYSELEY_DB_DIALECT === 'mysql') {
-  dialect = new MysqlDialect({
-    pool: createPool({
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD
-    })
-  });
-}
-if (process.env.KYSELEY_DB_DIALECT === 'planetscale') {
-  dialect = new PlanetScaleDialect({
-    url: process.env.DATABASE_URL,
-    fetch,
-  });
-}
+const { hosts, user, password, path } = new ConnectionString(process.env.DATABASE_URL);
 
 export const db = new Kysely<DB>({
-  dialect
+  dialect:
+    process.env.KYSELEY_DB_DIALECT === "mysql"
+      ? new MysqlDialect({
+          pool: createPool({
+            host: hosts && hosts[0].name,
+            user,
+            password,
+            database: path && path[0],
+          }),
+        })
+      : new PlanetScaleDialect({
+          url: process.env.DATABASE_URL,
+          fetch,
+        }),
 });
