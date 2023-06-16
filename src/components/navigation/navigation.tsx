da4de7357f5c6c9338318c620c9a6429655ac097
kotaro1574 <shake1574@gmail.com>
2023-06-17 01:54:01 +0900

    ⚡️: iconのサイズはNavigationItemで管理

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -7,9 +7,9 @@ export const Navigation = () => {
     <nav className="fixed bottom-0 z-50 w-full sm:sticky sm:w-auto sm:self-start">
       <div className="hidden sm:block  sm:px-3 sm:py-2">ロゴ</div>
       <ul className="flex cursor-pointer gap-1 border-t sm:flex-col sm:border-none">
-        <NavigationItem label="さがす" icon={<AiOutlineSearch className="text-2xl" />} />
-        <NavigationItem label="お気に入り" icon={<AiOutlineHeart className="text-2xl" />} />
-        <NavigationItem label="お買い物" icon={<AiOutlineShoppingCart className="text-2xl" />} />
+        <NavigationItem label="さがす" icon={<AiOutlineSearch />} />
+        <NavigationItem label="お気に入り" icon={<AiOutlineHeart />} />
+        <NavigationItem label="お買い物" icon={<AiOutlineShoppingCart />} />
       </ul>
     </nav>
   );
@@ -21,8 +21,8 @@ type NavigationItemProps = {
 };
 const NavigationItem = ({ label, icon }: NavigationItemProps) => {
   return (
-    <li className="flex w-full flex-col items-center gap-2 py-1 transition-colors duration-200  hover:bg-gray-200 sm:w-fit sm:flex-row sm:rounded-full sm:px-3 sm:py-2">
-      {icon}
+    <li className="flex w-full flex-col items-center gap-2 py-1 transition-colors duration-200 hover:bg-gray-200 sm:w-fit sm:flex-row sm:rounded-full sm:px-3 sm:py-2">
+      <div className="text-2xl">{icon}</div>
       <p className="text-xs sm:text-base">{label}</p>
     </li>
   );
