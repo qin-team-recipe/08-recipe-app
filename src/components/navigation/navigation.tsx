4e57e3a392db461db290765ea251c507f3f2633c
kotaro1574 <shake1574@gmail.com>
2023-06-17 11:03:54 +0900

    ⚡️: hrefを渡せるよう修正

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,6 @@
+import { UrlObject } from "url";
 import { ReactNode } from "react";
+import Link from "next/link";
 
 import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 
@@ -6,11 +8,11 @@ export const Navigation = () => {
   return (
     <nav className="fixed bottom-0 z-50 w-full sm:sticky sm:w-auto sm:self-start">
       <div className="hidden sm:block  sm:px-3 sm:py-2">ロゴ</div>
-      <ul className="flex cursor-pointer gap-1 border-t sm:flex-col sm:border-none">
-        <NavigationItem label="さがす" icon={<AiOutlineSearch />} />
-        <NavigationItem label="お気に入り" icon={<AiOutlineHeart />} />
-        <NavigationItem label="お買い物" icon={<AiOutlineShoppingCart />} />
-      </ul>
+      <div className="flex cursor-pointer gap-1 border-t sm:flex-col sm:border-none">
+        <NavigationItem label="さがす" icon={<AiOutlineSearch />} href="/" />
+        <NavigationItem label="お気に入り" icon={<AiOutlineHeart />} href="/" />
+        <NavigationItem label="お買い物" icon={<AiOutlineShoppingCart />} href="/" />
+      </div>
     </nav>
   );
 };
@@ -18,12 +20,16 @@ export const Navigation = () => {
 type NavigationItemProps = {
   label: string;
   icon: ReactNode;
+  href: UrlObject | __next_route_internal_types__.RouteImpl<string>;
 };
-const NavigationItem = ({ label, icon }: NavigationItemProps) => {
+const NavigationItem = ({ label, icon, href }: NavigationItemProps) => {
   return (
-    <li className="flex w-full flex-col items-center gap-2 py-1 transition-colors duration-200 hover:bg-gray-200 sm:w-fit sm:flex-row sm:rounded-full sm:px-3 sm:py-2">
+    <Link
+      href={href}
+      className="flex w-full flex-col items-center gap-2 py-1 transition-colors duration-200 hover:bg-gray-200 sm:w-fit sm:flex-row sm:rounded-full sm:px-3 sm:py-2"
+    >
       <div className="text-2xl">{icon}</div>
       <p className="text-xs sm:text-base">{label}</p>
-    </li>
+    </Link>
   );
 };
