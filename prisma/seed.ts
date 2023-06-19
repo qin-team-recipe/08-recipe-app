4dd8fa382ad8c2b74c394fae5ffc869cefaef5db
kimura0603 <k.kimura0603@gmail.com>
2023-06-19 23:11:59 +0900

    #15:userのschema追加途中

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,23 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import { userSeed } from "./seed/index";
 
 const prisma = new PrismaClient();
 const recipeData: Prisma.RecipeCreateInput[] = [{ name: "foo" }, { name: "bar" }, { name: "baz" }];
+//const userData: Prisma.UserCreateInput[] = userSeed;
+
 (async () => {
-  await Promise.all(
+  await Promise.all([
     recipeData.map(async (data) => {
       await prisma.recipe.create({ data });
+    }),
+    userSeed.map(async (data: Prisma.UserCreateInput) => {
+      await prisma.user.create({ data });
     })
-  );
+  ]);
   const recipes = await prisma.recipe.findMany();
   console.log(recipes);
+  const users = await prisma.user.findMany();
+  console.log('users', users);
 })()
   .then(async () => {
     await prisma.$disconnect();
--- src/types/db.ts
+++ src/types/db.ts
@@ -12,6 +12,15 @@ export type Recipe = {
   name: string;
   isPublic: Generated<number>;
 };
+export type User = {
+  id: string;
+  name: string;
+  user_type: string;
+  created_at: Generated<Timestamp>;
+  updated_at: Timestamp;
+  deleted_at: Timestamp | null;
+};
 export type DB = {
   Recipe: Recipe;
+  users: User;
 };
