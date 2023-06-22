7bfb5803cdf83984760e0f8832cc0d58c1d3748c
takeyu1013 <yuto1013@gmail.com>
2023-06-22 15:38:19 +0900

    💄

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -6,15 +6,15 @@ export default function Page() {
   return (
     <>
       <header className="bg-mauve-app flex items-center justify-center border-b border-b-mauve-6 px-4 py-3">
-        <h1 className="text-mauve-dim text-xl font-bold leading-6">買い物リスト</h1>
+        <h1 className="text-mauve-normal text-xl font-bold leading-6">買い物リスト</h1>
       </header>
       <main className="bg-mauve-app h-screen pt-5">
         <div className="flex items-center justify-between px-4">
-          <h2 className="text-mauve-dim font-bold">じぶんメモ</h2>
+          <h2 className="text-mauve-normal font-bold">じぶんメモ</h2>
           <span>+</span>
         </div>
         <div className="flex items-center justify-between px-4">
-          <h2 className="text-mauve-dim font-bold">グラタン</h2>
+          <h2 className="text-mauve-normal font-bold">グラタン</h2>
           <DropdownMenu>
             <DropdownMenuTrigger className="ml-auto block">
               <MoreVertical className="text-mauve-11" />
