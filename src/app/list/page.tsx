e3bcb1d21bce71d655aab46a8ab1649e33c0abda
takeyu1013 <yuto1013@gmail.com>
2023-06-22 17:55:12 +0900

    💄

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,4 +1,5 @@
-import { ChevronDown, ChevronUp, MoreVertical, Plus, ShoppingCart, Utensils } from "lucide-react";
+import { ChevronDown, ChevronUp, Plus, ShoppingCart, Utensils } from "lucide-react";
+import { TbCircleCheckFilled, TbDotsVertical, TbPlus } from "react-icons/tb";
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/features/list";
 
@@ -6,29 +7,33 @@ export default function Page() {
   return (
     <>
       <header className="bg-mauve-app flex items-center justify-center border-b border-b-mauve-6 px-4 py-3">
-        <h1 className="text-mauve-normal text-xl font-bold leading-6">買い物リスト</h1>
+        <h1 className="text-mauve-normal font-bold leading-6">買い物リスト</h1>
       </header>
-      <main className="bg-mauve-app h-screen pt-5">
-        <div className="flex flex-col gap-3 px-4">
-          <div className="flex items-center justify-between">
-            <h2 className="text-mauve-normal font-bold">じぶんメモ</h2>
-            <span>+</span>
+      <main className="bg-mauve-app flex h-screen flex-col gap-5 pt-5">
+        <div className="flex flex-col gap-y-3">
+          <div className="flex items-end justify-between px-4">
+            <h2 className="text-mauve-normal text-xl font-bold">じぶんメモ</h2>
+            <button className="p-1">
+              <TbPlus className="text-mauve-dim" size={20} />
+            </button>
           </div>
-          <ul>
+          <ul className="divide-y border-y border-mauve-6">
             {["チーズ", "マカロニ", "バジル"].map((name, index) => (
-              <div key={index} className="flex items-center justify-between">
-                <span>o</span>
-                <li className="text-mauve-normal mr-auto text-sm">{name}</li>
-                <span className="text-sm text-tomato-9">削除</span>
+              <div key={index} className="flex items-center justify-between gap-x-2 px-4 py-2">
+                <div className="py-1">
+                  <TbCircleCheckFilled className="text-mauve-8" size={24} />
+                </div>
+                <li className="text-mauve-normal mr-auto">{name}</li>
+                <span className="text-tomato-9">削除</span>
               </div>
             ))}
           </ul>
         </div>
-        <div className="flex items-center justify-between px-4">
-          <h2 className="text-mauve-normal font-bold">グラタン</h2>
+        <div className="flex items-end justify-between px-4">
+          <h2 className="text-mauve-normal text-xl font-bold">グラタン</h2>
           <DropdownMenu>
             <DropdownMenuTrigger className="ml-auto block">
-              <MoreVertical className="text-mauve-11" />
+              <TbDotsVertical className="text-mauve-dim" size={20} />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuItem className="gap-1">
