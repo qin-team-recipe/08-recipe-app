213554d099aa62b8bf0b5784fcdb106ce515b412
takeyu1013 <yuto1013@gmail.com>
2023-06-24 16:37:24 +0900

    🔧

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
+  theme: {
+    extend: {
+      colors: {
+        "whitea-13": "#ffffff",
+      },
+    },
+  },
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [require("tailwindcss-animate"), require("tailwindcss-radix-colors")],
 };
