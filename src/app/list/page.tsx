faae32985f7d413eda2174d078e072b5d9660ee0
takeyu1013 <yuto1013@gmail.com>
2023-07-02 23:43:44 +0900

    👔

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -18,6 +18,8 @@ export default async function Page() {
   if (!list.length) {
     await seed();
   }
+  const { id } = list.filter(({ recipeId }) => !recipeId)[0];
+  const memo = await db.selectFrom("Ingredient").selectAll().where("listId", "=", id).execute();
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
@@ -27,60 +29,62 @@ export default async function Page() {
         <ModeToggle />
       </header>
       <main className="bg-mauve-app flex flex-col gap-12 pt-5">
-        <MyList />
-        {list.map(async ({ id, name }) => {
-          const ingredients = await db
-            .selectFrom("Ingredient")
-            .select(["id", "name", "isChecked"])
-            .where("listId", "=", id)
-            .execute();
+        <MyList memo={memo} />
+        {list
+          .filter(({ recipeId }) => !!recipeId)
+          .map(async ({ id, name }) => {
+            const ingredients = await db
+              .selectFrom("Ingredient")
+              .select(["id", "name", "isChecked"])
+              .where("listId", "=", id)
+              .execute();
 
-          return (
-            <div key={id} className="flex flex-col gap-y-3">
-              <div className="flex items-end justify-between px-4">
-                <h2 className="text-mauve-normal text-xl font-bold">{name}</h2>
-                <DropdownMenu>
-                  <DropdownMenuTrigger className="-mb-0.5 -mr-0.5">
-                    <TbDotsVertical className="text-mauve-dim" size={20} />
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    {(
-                      [
-                        [TbToolsKitchen, "レシピ詳細を見る"],
-                        [TbChevronUp, "上に移動する"],
-                        [TbChevronDown, "下に移動する"],
-                        [TbPlus, "買うものを追加する"],
-                      ] as const
-                    ).map(([icon, text], index) => {
-                      return (
-                        <DropdownMenuItem key={index} className="gap-x-1">
-                          {((Icon) => (
-                            <Icon size={18} className="text-mauve-normal" />
-                          ))(icon)}
-                          {text}
-                        </DropdownMenuItem>
-                      );
-                    })}
-                    <DropdownMenuItem className="text-tomato-dim gap-1  focus:text-tomato-dim">
-                      <TbShoppingCartX className="text-tomato-dim" size={18} />
-                      リストから削除する
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+            return (
+              <div key={id} className="flex flex-col gap-y-3">
+                <div className="flex items-end justify-between px-4">
+                  <h2 className="text-mauve-normal text-xl font-bold">{name}</h2>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className="-mb-0.5 -mr-0.5">
+                      <TbDotsVertical className="text-mauve-dim" size={20} />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      {(
+                        [
+                          [TbToolsKitchen, "レシピ詳細を見る"],
+                          [TbChevronUp, "上に移動する"],
+                          [TbChevronDown, "下に移動する"],
+                          [TbPlus, "買うものを追加する"],
+                        ] as const
+                      ).map(([icon, text], index) => {
+                        return (
+                          <DropdownMenuItem key={index} className="gap-x-1">
+                            {((Icon) => (
+                              <Icon size={18} className="text-mauve-normal" />
+                            ))(icon)}
+                            {text}
+                          </DropdownMenuItem>
+                        );
+                      })}
+                      <DropdownMenuItem className="text-tomato-dim gap-1  focus:text-tomato-dim">
+                        <TbShoppingCartX className="text-tomato-dim" size={18} />
+                        リストから削除する
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+                <ul className="border-mauve-dim divide-mauve-dim divide-y border-y">
+                  {ingredients.map(({ id, name, isChecked }) => (
+                    <li key={id} className="flex items-center justify-between gap-x-2 px-4 py-2">
+                      <div className="flex items-center py-1 pr-2">
+                        <Checkbox value={isChecked} />
+                      </div>
+                      <label className="text-mauve-normal mr-auto">{name}</label>
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <ul className="border-mauve-dim divide-mauve-dim divide-y border-y">
-                {ingredients.map(({ id, name, isChecked }) => (
-                  <li key={id} className="flex items-center justify-between gap-x-2 px-4 py-2">
-                    <div className="flex items-center py-1 pr-2">
-                      <Checkbox value={isChecked} />
-                    </div>
-                    <label className="text-mauve-normal mr-auto">{name}</label>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        })}
+            );
+          })}
       </main>
     </ThemeProvider>
   );
