0ea088d1f6e0c843c81dce88071f968e5fb1d897
takeyu1013 <yuto1013@gmail.com>
2023-07-02 17:18:46 +0900

    🌱

--- src/lib/seed/list.ts
+++ src/lib/seed/list.ts
@@ -1,10 +1,34 @@
+import { Insertable } from "kysely";
+
 import { db } from "@/lib/kysely";
+import { Ingredient } from "@/types/db";
 
 export async function seed() {
-  await db
-    .insertInto("List")
-    .values([{ name: "foo" }, { name: "bar" }, { name: "baz" }])
-    .execute();
-  const listList = await db.selectFrom("List").selectAll().execute();
-  console.log(listList);
+  await (
+    [
+      ["グラタン", ["チーズ", "マカロニ", "ホワイトソース", "ブロッコリー"]],
+      ["グラタン", ["マカロニ", "ブロッコリー"]],
+      ["グラタン", ["キャベツ", "キャベツ", "キャベツ", "キャベツ"]],
+    ] as const
+  ).reduce(async (promise, [name, ingradients]) => {
+    await promise;
+    await db.insertInto("List").values({ name }).execute();
+    const result = await db.selectFrom("List").select("id").orderBy("createdAt", "desc").executeTakeFirst();
+    if (!result) {
+      return;
+    }
+    const ingradientData: Insertable<Ingredient>[] = ingradients.map((name) => ({
+      name,
+      listId: result.id,
+    }));
+    await db.insertInto("Ingredient").values(ingradientData).execute();
+  }, Promise.resolve());
+
+  const list = await db.selectFrom("List").selectAll().orderBy("createdAt").executeTakeFirst();
+  console.log(list);
+  if (!list) {
+    return;
+  }
+  const ingredients = await db.selectFrom("Ingredient").selectAll().where("listId", "=", list.id).execute();
+  console.table(ingredients);
 }
