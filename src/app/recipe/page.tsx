d36ce79be35a129483b0a800a3c2095d575c1f28
kotaro1574 <shake1574@gmail.com>
2023-07-09 13:53:30 +0900

    💄: レシピ詳細ページ作成

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -3,12 +3,16 @@
 import { useState } from "react";
 import Link from "next/link";
 
+import { TbCopy } from "react-icons/tb";
+
+import { Tabs } from "@/components/tabs/tabs";
+
 export default function Page() {
   const [isFavorite, setFavorite] = useState(false);
   const toggleFavorite = () => setFavorite(!isFavorite);
 
   return (
-    <main className={"h-screen"}>
+    <main>
       <div className={"aspect-square bg-tomato-3"}>jhsdkjhf</div>
       <div className={"space-y-3 p-4"}>
         <h1 className={"text-xl font-bold"}>レシピ</h1>
@@ -18,7 +22,7 @@ export default function Page() {
         <div className={"flex items-center gap-x-4"}>
           <Link className={"group flex items-center gap-x-1 text-sm"} href={"/"}>
             <div className={"h-5 w-5 rounded-full bg-tomato-5"} />
-            <div className={"sm:group-hover:underline"}>シェフ</div>
+            <div className={"sm:group-hover:underline"}>こたろシェフ</div>
           </Link>
           <div className={"text-sm"}>
             <span className={"mr-0.5 font-bold"}>1,234</span>
@@ -35,6 +39,55 @@ export default function Page() {
         >
           {isFavorite ? "お気に入り追加" : "お気に入りから削除"}
         </button>
+      </div>
+      <Tabs
+        tabList={[
+          {
+            name: "作り方",
+            href: `/recipe`,
+          },
+          {
+            name: "リンク",
+            href: `/`,
+          },
+        ]}
+      />
+      <div className={"divide-y divide-mauve-7 border-b border-mauve-7"}>
+        <div className={"flex gap-x-2 px-4 py-2"}>
+          <div className={"grid h-5 w-5 shrink-0 place-items-center rounded-full bg-tomato-10 text-sm text-tomato-1"}>
+            1
+          </div>
+          <div className={"flex-1"}>
+            <p className={"text-mauve-normal leading-snug"}>
+              用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。
+            </p>
+          </div>
+        </div>
+        <div className={"flex gap-x-2 px-4 py-2"}>
+          <div className={"grid h-5 w-5 shrink-0 place-items-center rounded-full bg-tomato-10 text-sm text-tomato-1"}>
+            2
+          </div>
+          <div className={"flex-1"}>
+            <p className={"text-mauve-normal leading-snug"}>
+              用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。
+            </p>
+          </div>
+        </div>
+        <div className={"flex gap-x-2 px-4 py-2"}>
+          <div className={"grid h-5 w-5 shrink-0 place-items-center rounded-full bg-tomato-10 text-sm text-tomato-1"}>
+            3
+          </div>
+          <div className={"flex-1"}>
+            <p className={"text-mauve-normal leading-snug"}>
+              用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={"flex cursor-pointer items-center justify-end gap-x-1 px-4 py-2 text-blue-11"}>
+        <TbCopy className={"text-base"} />
+        <p className={"text-xs"}>コピーする</p>
       </div>
     </main>
   );
