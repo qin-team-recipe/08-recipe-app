0c66876e31b039ae24906f1bdeac9ccad43363f3
kotaro1574 <shake1574@gmail.com>
2023-07-18 21:42:27 +0900

    Revert "💄: Buttonコンポーネント作ってみた"
    This reverts commit 8b5e09a78b4426f3e968f8ec52e95f582c2ae330.

--- src/features/recipes/components/recipe-favorite-button.tsx
+++ src/features/recipes/components/recipe-favorite-button.tsx
@@ -2,15 +2,19 @@
 
 import { useState } from "react";
 
-import { Button } from "@/components/button/button";
-
 export const RecipeFavoriteButton = () => {
   const [isFavorite, setFavorite] = useState(false);
   const toggleFavorite = () => setFavorite(!isFavorite);
+  const favoriteButtonToggleStyle = isFavorite
+    ? "bg-tomato-solid border-transparent"
+    : "border-tomato-normal text-tomato-dim";
 
   return (
-    <Button onClick={toggleFavorite} variant={isFavorite ? "outline" : "solid"}>
+    <button
+      onClick={toggleFavorite}
+      className={`w-full rounded-md  border px-3 py-1 text-sm ${favoriteButtonToggleStyle}`}
+    >
       {isFavorite ? "お気に入り追加" : "お気に入りから削除"}
-    </Button>
+    </button>
   );
 };
