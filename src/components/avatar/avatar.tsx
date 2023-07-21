509854357233df53a84917367aced9fd16f985ac
tanotaketo <s88.tano.taketo@gmail.com>
2023-07-21 15:49:21 +0900

    💄 背景色追加

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -32,7 +32,10 @@ const AvatarFallback = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
-    className={cn("bg-muted flex h-full w-full items-center justify-center rounded-full", className)}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-tomato-3 dark:bg-tomatodark-3",
+      className
+    )}
     {...props}
   />
 ));
