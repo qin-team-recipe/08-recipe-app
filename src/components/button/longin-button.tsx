b736a3bf66c004a80ff335da0a37cc1c5d944450
kotaro1574 <shake1574@gmail.com>
2023-07-22 22:27:58 +0900

    💄: LonginButtonコンポーネント作成

---
+++ src/components/button/longin-button.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps } from "react";
+
+import { AiOutlineGoogle } from "react-icons/ai";
+import { PiAppleLogoBold } from "react-icons/pi";
+import { match } from "ts-pattern";
+
+import { Button } from "@/components/button/button";
+
+type Props = Omit<ComponentProps<typeof Button>, "size"> & {
+  variant: "google" | "apple";
+};
+
+export const LonginButton = ({ variant, children, ...props }: Props) => {
+  const buttonStyle = match(variant)
+    .with("google", () => "bg-blue-9 hover:bg-blue-10 border-transparent text-whitea-13")
+    .with("apple", () => "bg-mauve-12 hover:opacity-90 border-transparent text-whitea-13")
+    .exhaustive();
+
+  const icon = match(variant)
+    .with("google", () => <AiOutlineGoogle className={"text-base"} />)
+    .with("apple", () => <PiAppleLogoBold className={"text-base"} />)
+    .exhaustive();
+
+  return (
+    <Button className={`flex items-center justify-center gap-1 ${buttonStyle}`} size={"md"} {...props}>
+      {icon}
+      <span className={"text-sm font-bold"}>{children}</span>
+    </Button>
+  );
+};
