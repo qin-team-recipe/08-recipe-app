67de88b4d64d5118dca07be994bffd9e26830a55
kotaro1574 <shake1574@gmail.com>
2023-07-23 12:01:25 +0900

    ⚡️: Buttonコンポートにdisabled時のstyle調整

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,9 +7,20 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 export const Button = ({ size = "sm", variant = "default", className, ...props }: Props) => {
   const buttonStyle = match(variant)
     .with("default", () => "")
-    .with("tomato", () => "bg-tomato-9 hover:bg-tomato-10 border-transparent text-whitea-13")
-    .with("tomato-outline", () => "border border-tomato-7 text-tomato-11 hover:bg-tomato-1 hover:border-tomato-8")
-    .with("black-outline", () => "border border-mauve-12 text-mauve-12 hover:bg-mauve-1")
+    .with(
+      "tomato",
+      () =>
+        "bg-tomato-9 hover:enabled:bg-tomato-10 border-transparent text-whitea-13 disabled:cursor-default disabled:opacity-50"
+    )
+    .with(
+      "tomato-outline",
+      () =>
+        "border border-tomato-7 text-tomato-11 hover:enabled:bg-tomato-1 hover:enabled:border-tomato-8 disabled:cursor-default disabled:opacity-50"
+    )
+    .with(
+      "black-outline",
+      () => "border border-mauve-12 text-mauve-12 hover:enabled:bg-mauve-1 disabled:cursor-default disabled:opacity-50"
+    )
     .exhaustive();
 
   const buttonSize = match(size)
