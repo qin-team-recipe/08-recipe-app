bac56e46fca05eca2568841c3b6d63c40f69130e
takeyu1013 <yuto1013@gmail.com>
2023-07-28 23:09:23 +0900

    🗃️ ユーザーテーブルを部分的に開放

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,5 +1,7 @@
 import type { ColumnType } from "kysely";
 
+import type { UserType } from "./enums";
+
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
@@ -19,7 +21,16 @@ export type List = {
   index: number;
   recipeId: string | null;
 };
+export type User = {
+  id: Generated<string>;
+  name: string;
+  userType: Generated<UserType>;
+  createdAt: Generated<Timestamp>;
+  updatedAt: Generated<Timestamp>;
+  deletedAt: Timestamp | null;
+};
 export type DB = {
   Ingredient: Ingredient;
   List: List;
+  User: User;
 };
