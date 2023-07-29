86eb8fb9f36e3ca7862952d85d093f94f2e60a0e
kotaro1574 <shake1574@gmail.com>
2023-07-29 21:44:06 +0900

    💬: 文言修正

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <main className="grid gap-4 p-4">
       <Link href={"/components/button"} className={"hover:text-blue-11 hover:underline"}>
-        Buttonコンポート
+        Buttonコンポーネント
       </Link>
       <Link href={"/components/tab/1"} className={"hover:text-blue-11 hover:underline"}>
-        Tabコンポート
+        Tabコンポーネント
       </Link>
     </main>
   );
