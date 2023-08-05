746db410a07d6f1fbbb4f4ec97fd037c39b65408
tanotaketo <s88.tano.taketo@gmail.com>
2023-08-05 18:00:18 +0900

    💄 VerticalRecipeListコンポーネント作成

---
+++ src/components/vertical-recipe-list/vertical-recipe-list.tsx
@@ -0,0 +1,15 @@
+import { RecipeListItem } from "@/components/recipe-list-item/recipe-list-item";
+
+export const VerticalRecipeList = <T extends string>({ recipeList }: { recipeList: RecipeListItem<T>[] }) => {
+  return (
+    <div className="grid grid-cols-2 gap-x-3 gap-y-4">
+      {recipeList.map((recipeListItem) => {
+        return (
+          <div className="w-full" key={recipeListItem.id}>
+            <RecipeListItem recipeListItem={recipeListItem} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
