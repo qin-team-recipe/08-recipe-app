a6b3f1452d69a4b372c785fdd1c458e044a3ede6
tanotaketo <s88.tano.taketo@gmail.com>
2023-08-06 16:55:59 +0900

    💄 新着レシピ、人気レシピ追加

--- src/app/chef/[id]/popular/page.tsx
+++ src/app/chef/[id]/popular/page.tsx
@@ -6,7 +6,7 @@ export default function Page({ params }: { params: { id: string } }) {
       ({
         id: i,
         href: `/recipe`,
-        image: "/images/recipe_01.png",
+        image: "/images/recipe_02.png",
         name: "自家燻製したノルウェーサーモンと帆立貝柱のムースのキャベツ包み蒸し 生雲丹とパセリのヴルーテ",
         chefName: "中々田中ジェフシェフの超々最長ミシシッピレシピ収集",
         favoriteCount: 1234,
