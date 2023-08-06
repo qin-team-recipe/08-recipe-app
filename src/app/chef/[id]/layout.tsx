e10011ac220e08c1a30f1ef69919bba2d50dd0c8
tanotaketo <s88.tano.taketo@gmail.com>
2023-08-06 16:54:47 +0900

    💄 tab追加、全体レイアウト修正

--- src/app/chef/[id]/layout.tsx
+++ src/app/chef/[id]/layout.tsx
@@ -1,9 +1,22 @@
 import { ChefDetail } from "@/components/chef-detail/chef-detail";
+import { Tabs } from "@/components/tabs/tabs";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ params, children }: { params: { id: number }; children: React.ReactNode }) {
   return (
-    <main>
+    <main className="flex w-full flex-col items-start gap-5 self-stretch">
       <ChefDetail />
+      <Tabs
+        tabList={[
+          {
+            name: "新着レシピ",
+            href: `/chef/${params.id}`,
+          },
+          {
+            name: "人気レシピ",
+            href: `/chef/${params.id}/popular`,
+          },
+        ]}
+      ></Tabs>
       {children}
     </main>
   );
