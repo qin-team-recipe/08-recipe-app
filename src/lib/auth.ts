0990df518bb3fd99139e29084b3d973506055fbc
takeyu1013 <yuto1013@gmail.com>
2023-08-20 00:10:39 +0900

    🏷️ アダプターの型を拡張して型エラーを一部修正

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,13 @@
 import { Database, KyselyAdapter } from "@auth/kysely-adapter";
 import { Kysely } from "kysely";
 import { AuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@/lib/kysely";
 
 export const authOptions: AuthOptions = {
   // HACK: 型エラーが発生するため、暫定的に対応（ライブラリ側の不具合の可能性あり）
-  adapter: KyselyAdapter(db as unknown as Kysely<Database>) as Adapter,
+  adapter: KyselyAdapter(db as unknown as Kysely<Database>),
   session: {
     strategy: "jwt",
   },
