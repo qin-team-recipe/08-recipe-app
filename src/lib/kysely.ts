dd3c606e64b390de8c93fdda125ecc7e9d428074
takeyu1013 <yuto1013@gmail.com>
2023-08-19 20:56:23 +0900

    🗃️ Kysely adapter及びprisma-kysely用にDB接続周りを変更

--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -1,5 +1,6 @@
+import { Codegen, KyselyAuth } from "@auth/kysely-adapter";
 import { ConnectionString } from "connection-string";
-import { Kysely, MysqlDialect } from "kysely";
+import { MysqlDialect } from "kysely";
 import { PlanetScaleDialect } from "kysely-planetscale";
 import { createPool } from "mysql2";
 import { fetch } from "undici";
@@ -8,19 +9,22 @@ import { DB } from "@/types/db";
 
 const { hosts, user, password, path } = new ConnectionString(process.env.DATABASE_URL);
 
-const dialect =
-  process.env.KYSELEY_DB_DIALECT === "mysql"
-    ? new MysqlDialect({
-        pool: createPool({
-          host: hosts && hosts[0].name,
-          user,
-          password,
-          database: path && path[0],
-        }),
-      })
-    : new PlanetScaleDialect({
-        url: process.env.DATABASE_URL,
-        fetch,
-      });
+const dialect = (() => {
+  if (process.env.KYSELEY_DB_DIALECT !== "mysql")
+    return new PlanetScaleDialect({
+      url: process.env.DATABASE_URL,
+      fetch,
+    });
+  return new MysqlDialect({
+    pool: createPool({
+      host: hosts && hosts[0].name,
+      user,
+      password,
+      database: path && path[0],
+    }),
+  });
+})();
 
-export const db = new Kysely<DB>({ dialect });
+export const db = new KyselyAuth<DB, Codegen>({
+  dialect,
+});
