89a5fb53cfdda6c22f1fe46048528fdcc983fcc2
takeyu1013 <yuto1013@gmail.com>
2023-08-19 10:44:42 +0900

    🛂 試験的にログアウト処理を追加

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Session } from "next-auth";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 import { Button } from "@/components/button/button";
 
@@ -17,6 +17,14 @@ export default function Login({ session }: { session: Session | null }) {
         Googleでログイン
       </Button>
       <p>{session && `${session.user?.name} でログイン中`}</p>
+      <Button
+        variant="blackOutline"
+        onClick={() => {
+          signOut();
+        }}
+      >
+        ログアウト
+      </Button>
     </main>
   );
 }
