932b008c9f9e4d4051be9d45d37fe039c564dcf6
takeyu1013 <yuto1013@gmail.com>
2023-08-19 21:04:22 +0900

    💩 型エラー発生のため、暫定的に強制型変換でKysely adapterに対応

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,17 @@
+import { Database, KyselyAdapter } from "@auth/kysely-adapter";
+import { Kysely } from "kysely";
 import { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
+import { db } from "@/lib/kysely";
+
 export const authOptions: AuthOptions = {
+  // HACK: 型エラーが発生するため、暫定的に対応（ライブラリ側の不具合の可能性あり）
+  adapter: KyselyAdapter(db as unknown as Kysely<Database>) as Adapter,
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
