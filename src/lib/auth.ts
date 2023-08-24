7ac6ab20f89332bafcb76e78231ce08fb31e884c
takeyu1013 <yuto1013@gmail.com>
2023-08-24 22:40:39 +0900

    ♻️ 認証用設定のリファクタリング

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,18 @@
-import { Database, KyselyAdapter } from "@auth/kysely-adapter";
-import { Kysely } from "kysely";
+import { Codegen, Database, KyselyAdapter, KyselyAuth } from "@auth/kysely-adapter";
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-import { db } from "@/lib/kysely";
+import { dialect } from "@/lib/kysely";
 
 export const authOptions: AuthOptions = {
-  // HACK: 型エラーが発生するため、暫定的に対応（ライブラリ側の不具合の可能性あり）
-  adapter: KyselyAdapter(db as unknown as Kysely<Database>),
+  adapter: KyselyAdapter(new KyselyAuth<Database, Codegen>({ dialect })),
   session: {
     strategy: "jwt",
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
   pages: {
--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -7,24 +7,24 @@ import { fetch } from "undici";
 
 import { DB } from "@/types/db";
 
-const { hosts, user, password, path } = new ConnectionString(process.env.DATABASE_URL);
+export const dialect = (() => {
+  if (process.env.KYSELEY_DB_DIALECT === "mysql") {
+    const { hosts, user, password, path } = new ConnectionString(process.env.DATABASE_URL);
 
-const dialect = (() => {
-  if (process.env.KYSELEY_DB_DIALECT !== "mysql")
-    return new PlanetScaleDialect({
-      url: process.env.DATABASE_URL,
-      fetch,
+    return new MysqlDialect({
+      pool: createPool({
+        host: hosts && hosts[0].name,
+        user,
+        password,
+        database: path && path[0],
+      }),
     });
-  return new MysqlDialect({
-    pool: createPool({
-      host: hosts && hosts[0].name,
-      user,
-      password,
-      database: path && path[0],
-    }),
+  }
+
+  return new PlanetScaleDialect({
+    url: process.env.DATABASE_URL,
+    fetch,
   });
 })();
 
-export const db = new KyselyAuth<DB, Codegen>({
-  dialect,
-});
+export const db = new KyselyAuth<DB, Codegen>({ dialect });
