f2d9dd1c33a1dc6ea0f92a84b4aac00d4ba382aa
kentakimura <k.kimura0603@gmail.com>
2023-08-31 19:13:02 +0900

    ダイアログ　アニメーション微修正

--- src/components/alert-dialog/alert-dialog.tsx
+++ src/components/alert-dialog/alert-dialog.tsx
@@ -44,8 +44,7 @@ const AlertDialogContent = React.forwardRef<
         "translate-x-[-50%] duration-200 md:translate-y-[-50%]",
         "grid gap-4",
         "w-full max-w-lg rounded-lg border bg-whitea-13 p-6 shadow-lg",
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
-        // "bg-background fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[0%] data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-bottom-[0%] data-[state=open]:slide-in-from-left-1/2 md:data-[state=closed]:slide-out-to-top-[48%] md:data-[state=open]:slide-in-from-top-[48%]",
         className,
       )}
       {...props}
