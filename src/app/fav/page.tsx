4b97dde6af2676ae6734adc7d46a77e47ee9367f
n-tsukiya <tkmaru555@gmail.com>
2023-09-05 20:41:46 +0900

    ♻️ 同じ記述の部分を共通化

--- src/app/fav/page.tsx
+++ src/app/fav/page.tsx
@@ -9,20 +9,6 @@ import { authOptions } from "@/lib/auth";
 export default async function Page() {
   const session = await getServerSession(authOptions);
 
-  if (!session)
-    return (
-      <main className="min-h-screen w-full text-mauve-12">
-        <div className="flex h-12 items-center justify-between gap-x-4 border-b border-mauve-6 px-4 py-3">
-          <Link href="/settings">
-            <TbMenu className="h-6 w-6" />
-          </Link>
-          <h1 className="text-xl font-bold leading-6">お気に入り</h1>
-          <div className="h-6 w-6"></div>
-        </div>
-        <Login imgSrc="/images/fav-login.png" />
-      </main>
-    );
-
   return (
     <main className="min-h-screen w-full text-mauve-12">
       <div className="flex h-12 items-center justify-between gap-x-4 border-b border-mauve-6 px-4 py-3">
@@ -32,7 +18,7 @@ export default async function Page() {
         <h1 className="text-xl font-bold leading-6">お気に入り</h1>
         <div className="h-6 w-6"></div>
       </div>
-      <p>{session && `${session.user?.name} でログイン中`}</p>
+      {session ? <p>{session && `${session.user?.name} でログイン中`}</p> : <Login imgSrc="/images/fav-login.png" />}
     </main>
   );
 }
