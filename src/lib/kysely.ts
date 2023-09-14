84424beeef29f3a8de4d6bec5ca5f8d58368b569
kentakimura <k.kimura0603@gmail.com>
2023-09-14 23:03:00 +0900

    🔧 kyselyのQueryログを開発環境で出すようにした

--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -1,6 +1,20 @@
 import { Codegen, KyselyAuth } from "@auth/kysely-adapter";
+import { LogEvent } from "kysely";
 
 import { dialect } from "@/lib/kysely-node";
 import { DB } from "@/types/db";
 
-export const db = new KyselyAuth<DB, Codegen>({ dialect });
+const kyselyConfig = {
+  dialect,
+  ...(process.env.NODE_ENV === "development" && {
+    log: (event: LogEvent) => {
+      if (event.level == "query") {
+        const q = event.query;
+        const time = Math.round(event.queryDurationMillis * 100) / 100;
+        console.log(`\u001b[32mkysely:sql\u001b[0m [${q.sql}] parameters: [${q.parameters}] time: ${time}`);
+      }
+    },
+  }),
+};
+
+export const db = new KyselyAuth<DB, Codegen>(kyselyConfig);
