5815734bd7720752c5078f9fbcfd688ad01e120e
kentakimura <k.kimura0603@gmail.com>
2023-09-16 01:38:50 +0900

    RecipeListItemコンポーネントを仕様に合わせて修正

--- src/app/components/vertical-recipe-list/page.tsx
+++ src/app/components/vertical-recipe-list/page.tsx
@@ -1,18 +1,22 @@
-import { VerticalRecipeList } from "@/components/vertical-recipe-list/vertical-recipe-list";
+import { randomUUID, UUID } from "crypto";
+
+import { RecipeListItemType, VerticalRecipeList } from "@/features/recipes/";
 
 export default function Page() {
-  const recipeList = Array.from({ length: 10 }).map(
-    (_, i) =>
-      ({
-        id: 1,
-        href: `/recipe`,
-        image: "/images/recipe_01.png",
-        name: "自家燻製したノルウェーサーモンと帆立貝柱のムースのキャベツ包み蒸し 生雲丹とパセリのヴルーテ",
-        chefName: "中々田中ジェフシェフの超々最長ミシシッピレシピ収集",
-        favoriteCount: 1234,
-        isPublic: true,
-      } as const)
-  );
+  const recipeList: RecipeListItemType[] = Array.from({ length: 10 }).map(() => {
+    return {
+      id: randomUUID() as UUID,
+      userId: randomUUID() as UUID,
+      imgSrc: "/recipe_01.png",
+      name: "自家燻製したノルウェーサーモンと帆立貝柱のムースのキャベツ包み蒸し 生雲丹とパセリのヴルーテ",
+      description: "中々田中ジェフシェフの超々最長ミシシッピレシピ収集",
+      favoriteCount: 1234,
+      servings: 4,
+      isPublic: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  });
 
   return (
     <div className="px-4">
--- src/features/recipes/index.ts
+++ src/features/recipes/index.ts
@@ -1,7 +1,13 @@
 // components:
 export * from "./components/recipe-favorite-button";
 export * from "./components/recipe-step";
+// export * from "./components/recipe-list/recipe-list-infinite-scroll";
+export * from "./components/recipe-list/vertical-recipe-list";
+export * from "./components/recipe-list/recipe-list-item";
 
 // lib
 export * from "./lib/action";
 export * from "./lib/get-recipes";
+
+//type
+export * from "./types/";
