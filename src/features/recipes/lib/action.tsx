8a72891d71d136a988dac433fe7a5b25a547f2a2
kentakimura <k.kimura0603@gmail.com>
2023-09-18 01:18:24 +0900

    不要なServerActions関数削除

--- src/features/recipes/lib/action.tsx
+++ src/features/recipes/lib/action.tsx
@@ -1,11 +1,7 @@
 "use server";
 
-import { ReactNode } from "react";
-
-import { getRecipesFavoritedRecently, getRecipesWithFavoriteCount } from "@/features/recipes";
-
-import { VerticalRecipeList } from "../components/recipe-list/vertical-recipe-list";
 import { RecipeListItem } from "../types";
+import { getRecipesFavoritedRecently, getRecipesWithFavoriteCount } from "./get-recipes";
 
 export async function fetchRecipesWithFavoriteCount({
   search,
@@ -27,46 +23,4 @@ export async function fetchRecipesFavoritedRecently({
 }) {
   const recipes: RecipeListItem[] = await getRecipesFavoritedRecently({ query: search, page });
   return recipes;
-}
-
-//TODO: Promise<typeof VerticalRecipeList>よりもっと良い書き方ありそう
-export async function loadRecipeElement(contents: RecipeListItem[]): Promise<JSX.Element> {
-  return <VerticalRecipeList recipeList={contents} />;
-}
-
-export async function loadRecipeComponentWithFetchRecipeData({
-  contents,
-  search,
-  page = 1,
-}: {
-  contents: RecipeListItem[];
-  search?: string;
-  page?: number;
-}): Promise<{ contentsUpdated: RecipeListItem[]; contentElementUpdated: ReactNode }> {
-  const recipesLoadedByScroll: RecipeListItem[] = await getRecipesWithFavoriteCount({ query: search, page });
-  const recipesUpdated = [...(contents?.length ? contents : []), ...recipesLoadedByScroll];
-  const contentElement = await loadRecipeElement(recipesUpdated);
-  return {
-    contentsUpdated: recipesUpdated,
-    contentElementUpdated: contentElement,
-  };
-}
-
-export async function loadRecipeComponentWithFetchRecipeDataFavoritedRecently({
-  contents,
-  search,
-  page = 1,
-}: {
-  contents: RecipeListItem[];
-  search?: string;
-  page?: number;
-}): Promise<{ contentsUpdated: RecipeListItem[]; contentElementUpdated: ReactNode }> {
-  console.log("loadRecipeComponentWithFetchRecipeDataFavoritedRecently");
-  const recipesLoadedByScroll: RecipeListItem[] = await getRecipesFavoritedRecently({ query: search, page });
-  const recipes = [...(contents?.length ? contents : []), ...recipesLoadedByScroll];
-  const contentElement = await loadRecipeElement(recipes);
-  return {
-    contentsUpdated: recipes,
-    contentElementUpdated: contentElement,
-  };
 }
