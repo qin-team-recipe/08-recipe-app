e1ab6e615d165cd35d98c281a1f5015548c54e69
takeyu1013 <yuto1013@gmail.com>
2023-09-17 13:02:17 +0900

    ♻️ UIのリファクタリング

--- src/app/components/vertical-chef-list-item/page.tsx
+++ src/app/components/vertical-chef-list-item/page.tsx
@@ -1,10 +1,10 @@
-import { randomUUID, UUID } from "crypto";
+import { randomUUID } from "crypto";
 
 import { ChefListItemWithRecipeCountType, VerticalChefListItem } from "@/features/chefs/";
 
 export default function Page() {
   const chef: ChefListItemWithRecipeCountType = {
-    id: randomUUID() as UUID,
+    id: randomUUID(),
     name: "山田シェフ",
     profileText: `初の絵本出版！ 『 まねっこシェフ』 ・ふわふわ！スクランブルエッグ ・にぎにぎ！おにぎり 主婦の友社より３月３日、２冊同時発売！ 絶賛発売中！`,
     image: "/chefs/268215431924014868267858525012700090910279903691.jpg",
--- src/app/components/vertical-chef-list/page.tsx
+++ src/app/components/vertical-chef-list/page.tsx
@@ -1,4 +1,4 @@
-import { randomUUID, UUID } from "crypto";
+import { randomUUID } from "crypto";
 
 import { VerticalChefList } from "@/features/chefs/";
 
@@ -7,7 +7,7 @@ export default function page() {
     const dummyChefList = [];
     for (let i = 1; i <= count; i++) {
       dummyChefList.push({
-        id: randomUUID() as UUID,
+        id: randomUUID(),
         name: "山田シェフ",
         profileText: `初の絵本出版！ 『 まねっこシェフ』 ・ふわふわ！スクランブルエッグ ・にぎにぎ！おにぎり 主婦の友社より３月３日、２冊同時発売！ 絶賛発売中！`,
         image: "/chefs/268215431924014868267858525012700090910279903691.jpg",
