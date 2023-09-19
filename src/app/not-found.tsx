18151f44444d3d5f6a6f252847b97e9a58e4899a
n-tsukiya <tkmaru555@gmail.com>
2023-09-19 20:02:34 +0900

    ✨ 404ページの作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center gap-y-5 pt-5">
+      <div className="">
+        <Image src="/images/404.png" width={200} height={200} alt="" />
+      </div>
+      <div className="">
+        <p className="text-center font-bold">お探しのページが見つかりません</p>
+        <p className="mt-3 text-center text-mauve-12">
+          あなたがアクセスしたページは削除されたかURLが変更されているため表示することができません。
+        </p>
+      </div>
+    </div>
+  );
+}
