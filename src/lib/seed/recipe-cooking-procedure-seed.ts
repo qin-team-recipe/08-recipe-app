a0bb72844999f2290662ea8d3ad54793e6cf8ab3
kentakimura <k.kimura0603@gmail.com>
2023-09-20 19:28:01 +0900

    RecipeCookingProcedureとRecipeLinkSeedのシーダーをリファクタリング

--- src/lib/seed/recipe-cooking-procedure-seed.ts
+++ src/lib/seed/recipe-cooking-procedure-seed.ts
@@ -18,337 +18,160 @@ export const recipeCookingProcedureSeed = async () => {
     }),
   );
 
-  const recipeCookingProcedures = (
+  const recipeCookingProceduresArray = [
     [
-      {
-        name: "玉ねぎをみじん切りにし、レンジで4~5分加熱するか、フライパンで炒める。",
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        index: 0,
-      },
-      {
-        name: "ひき肉に塩を加え粘りがでるまでよく混ぜ、ハンバーグの材料すべてを入れて手早く混ぜる。",
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        index: 1,
-      },
-      {
-        name: "２〜３個にわけて叩きながら空気を抜き、楕円形にまとめる。真ん中をくぼませる。",
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        index: 2,
-      },
-      {
-        name: "フライパンで両面軽く焦げ目がつくまで焼き、水50mlを加えフタをして蒸し焼きにする。",
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        index: 3,
-      },
-      {
-        name: "竹串などを刺して肉汁が透明になっていれば、フタを外して水分を飛ばす。ハンバーグを取り出す。",
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        index: 4,
-      },
-      {
-        name: "空いたフライパンにそのままソースの材料を入れて煮る。ソースがあたたまったらハンバーグにかけ、完成。",
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        index: 5,
-      },
-      {
-        name: "明太子は薄皮から取り出し、身をほぐしておきます。イタリアンパセリは刻んでおきます。",
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        index: 0,
-      },
-      {
-        name: "ニンニクはみじん切りにします。",
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        index: 1,
-      },
-      {
-        name: "鍋に1、オリーブオイルを入れ中火で熱し、ニンニクの香りがしてきたら弱火で4分程、途中で混ぜながら加熱します。ニンニクに焼き色が付いたらキッチンペーパーを敷いたザルで漉し、ニンニクオイルとニンニクチップに分けます。ニンニクオイルは粗熱を取ります。",
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        index: 2,
-      },
-      {
-        name: "お湯を沸騰させた鍋に塩を入れて、スパゲティをパッケージの表記より1分短くゆで、ゆで汁を60ml取り分け、お湯を切ります。",
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        index: 3,
-      },
-      {
-        name: "ボウルに明太子ソースの材料と、3のニンニクオイルを15g、4のスパゲティを入れ混ぜ合わせます。ゆで汁を2回に分けて入れ、都度手早く混ぜます。",
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        index: 4,
-      },
-      {
-        name: "器に盛り付け、3のニンニクチップ、イタリアンパセリ、粗挽き黒こしょうをかけ完成です。",
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        index: 5,
-      },
-      {
-        name: "大葉は軸を切り落としておきます。しめじ、まいたけは石づきを切り落としておきます。",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 0,
-      },
-      {
-        name: "大葉は千切りにします。",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 1,
-      },
-      {
-        name: "しめじ、まいたけはほぐします。",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 2,
-      },
-      {
-        name: "ボウルに(A)を入れ混ぜ合わせます。",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 3,
-      },
-      {
-        name: "鍋にお湯を沸かし、塩、スパゲティを入れ、パッケージの表記時間より1分短くゆで、お湯を切ります。",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 4,
-      },
-      {
-        name: "中火で熱したフライパンに無塩バターを溶かし、3、塩、白こしょうを入れしめじがしんなりとするまで炒め、火から下ろします。",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 5,
-      },
-      {
-        name: "4に5、6加えて和えます。器に盛りつけ1をのせたら完成です",
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        index: 6,
-      },
-      {
-        name: "じゃがいもと人参は大きめの乱切りに、玉ねぎはくし切り、白滝は洗って適当に切ります。",
-        recipeId: recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
-        index: 0,
-      },
-      {
-        name: "鍋に油を大２入れ強火にし、肉を入れ色が変わるまで炒めたら１も入れ軽く炒めます♪",
-        recipeId: recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
-        index: 1,
-      },
-      {
-        name: "煮汁の材料を入れ沸騰したら灰汁を取り白滝を入れ混ぜ中強火のまま落し蓋をし煮汁が無くなるまで約20分程煮詰めます。",
-        recipeId: recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
-        index: 2,
-      },
-      {
-        name: "10分煮詰めた所で一度混ぜ、再度落し蓋をして煮汁がなくなるまで様子を見ながら更に10分位煮詰めていきます。",
-        recipeId: recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
-        index: 3,
-      },
-      {
-        name: "煮汁が無くなったら火を止め落し蓋をしたまま10分間、蒸らします♪味が更に染み込みます。これで完成！",
-        recipeId: recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
-        index: 4,
-      },
-      {
-        name: `ボウルを３つ用意します。
+      recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
+      [
+        "玉ねぎをみじん切りにし、レンジで4~5分加熱するか、フライパンで炒める。",
+        "ひき肉に塩を加え粘りがでるまでよく混ぜ、ハンバーグの材料すべてを入れて手早く混ぜる。",
+        "２〜３個にわけて叩きながら空気を抜き、楕円形にまとめる。真ん中をくぼませる。",
+        "フライパンで両面軽く焦げ目がつくまで焼き、水50mlを加えフタをして蒸し焼きにする。",
+        "竹串などを刺して肉汁が透明になっていれば、フタを外して水分を飛ばす。ハンバーグを取り出す。",
+        "空いたフライパンにそのままソースの材料を入れて煮る。ソースがあたたまったらハンバーグにかけ、完成。",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("最高の明太子パスタ"),
+      [
+        "明太子は薄皮から取り出し、身をほぐしておきます。イタリアンパセリは刻んでおきます。",
+        "ニンニクはみじん切りにします。",
+        "鍋に1、オリーブオイルを入れ中火で熱し、ニンニクの香りがしてきたら弱火で4分程、途中で混ぜながら加熱します。ニンニクに焼き色が付いたらキッチンペーパーを敷いたザルで漉し、ニンニクオイルとニンニクチップに分けます。ニンニクオイルは粗熱を取ります。",
+        "お湯を沸騰させた鍋に塩を入れて、スパゲティをパッケージの表記より1分短くゆで、ゆで汁を60ml取り分け、お湯を切ります。",
+        "ボウルに明太子ソースの材料と、3のニンニクオイルを15g、4のスパゲティを入れ混ぜ合わせます。ゆで汁を2回に分けて入れ、都度手早く混ぜます。",
+        "器に盛り付け、3のニンニクチップ、イタリアンパセリ、粗挽き黒こしょうをかけ完成です。",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
+      [
+        "大葉は千切りにします。",
+        "大葉は軸を切り落としておきます。しめじ、まいたけは石づきを切り落としておきます。",
+        "しめじ、まいたけはほぐします。",
+        "ボウルに(A)を入れ混ぜ合わせます。",
+        "鍋にお湯を沸かし、塩、スパゲティを入れ、パッケージの表記時間より1分短くゆで、お湯を切ります。",
+        "中火で熱したフライパンに無塩バターを溶かし、3、塩、白こしょうを入れしめじがしんなりとするまで炒め、火から下ろします。",
+        "4に5、6加えて和えます。器に盛りつけ1をのせたら完成です",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
+      [
+        "じゃがいもと人参は大きめの乱切りに、玉ねぎはくし切り、白滝は洗って適当に切ります。",
+        "鍋に油を大２入れ強火にし、肉を入れ色が変わるまで炒めたら１も入れ軽く炒めます♪",
+        "煮汁の材料を入れ沸騰したら灰汁を取り白滝を入れ混ぜ中強火のまま落し蓋をし煮汁が無くなるまで約20分程煮詰めます。",
+        "10分煮詰めた所で一度混ぜ、再度落し蓋をして煮汁がなくなるまで様子を見ながら更に10分位煮詰めていきます。",
+        "煮汁が無くなったら火を止め落し蓋をしたまま10分間、蒸らします♪味が更に染み込みます。これで完成！",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
+      [
+        `ボウルを３つ用意します。
         大きめのボウルは湯煎用と氷水用
         に。一回り小さめのボウルにチョコを入れて湯煎で溶かします。
         `,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 0,
-      },
-      {
-        name: `チョコが溶けたら生クリームを入れ良く混ぜます。
+        `チョコが溶けたら生クリームを入れ良く混ぜます。
         混ざったら湯煎から氷水のボウルに変え良く混ぜて冷やします。
         `,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 1,
-      },
-      {
-        name: `冷えて来たら練乳も入れ泡立て器で角が立つまで泡立てます。
+        `冷えて来たら練乳も入れ泡立て器で角が立つまで泡立てます。
         出来たクリームは絞り袋に入れて冷蔵庫で少し冷やします。
         `,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 2,
-      },
-      {
-        name: `トレーにラップを敷き
+        `トレーにラップを敷き
         こんな感じで絞り出したらラップをかけ、冷凍庫に入れ固まるまで1時間程待ちます。
         `,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 3,
-      },
-      {
-        name: `クリームが凍ったら
+        `クリームが凍ったら
         ラップにあんこを小さじ1.5広げ
         凍ったクリームを乗せ手早くラップを丸めて口を捻り再度冷凍庫へ入れる
         `,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 4,
-      },
-      {
-        name: `大体、冷凍庫に1時間以上入れて凍らせます。
+        `大体、冷凍庫に1時間以上入れて凍らせます。
         ここでしっかり凍らせてね！
         ※求肥に包む際に柔らか過ぎて包めなくなるので！！
         `,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 5,
-      },
-      {
-        name: `ここまで前日に仕込んで置いても
+        `ここまで前日に仕込んで置いても
         作業が楽にやりやすくなりますよ！`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 6,
-      },
-      {
-        name: `凍らせてる間に求肥作り。
+        `凍らせてる間に求肥作り。
         耐熱ボウルにだんご粉と砂糖を入れゴムヘラで混ぜたら水も入れ更に良くまぜます。`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 7,
-      },
-      {
-        name: `混ざったら軽くラップして800wで2分チン。良く混ぜたら
+        `混ざったら軽くラップして800wで2分チン。良く混ぜたら
         またラップをして2分チン。
         3回位やると透明感が出てきます。`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 8,
-      },
-      {
-        name: `3回チンしたら、水あめを入れて更に良く混ぜます。
+        `3回チンしたら、水あめを入れて更に良く混ぜます。
         そして最後にもう一度2分チン。
         良く混ぜます。`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 9,
-      },
-      {
-        name: `バットに抹茶を敷き広げ
+        `バットに抹茶を敷き広げ
         求肥を平らに広げて
         上からも抹茶を振りかけて
         スケッパーで30等分に切り分け冷まします。`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 10,
-      },
-      {
-        name: `6のクリームあんこが凍ったら
+        `6のクリームあんこが凍ったら
         10の求肥を平らに広げて乗せます。`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 11,
-      },
-      {
-        name: `手早く包み口をつまんで閉じます。求肥が手についてベタつく時は抹茶を転がしながら付けてやるとやり易いです。`,
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        index: 12,
-      },
-      {
-        name: `ブロック肉は半分に切ります。
+        `手早く包み口をつまんで閉じます。求肥が手についてベタつく時は抹茶を転がしながら付けてやるとやり易いです。`,
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
+      [
+        `ブロック肉は半分に切ります。
         鍋に肉を入れ肉が被る程度に水をたっぷり入れます。`,
-        recipeId: recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
-        index: 0,
-      },
-      {
-        name: "生姜はスライス、長ネギの青い部分と玉ねぎを半分に切り鍋に入れ沸騰したらアクを取り出し弱火で1時間は煮込みます。",
-        recipeId: recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
-        index: 1,
-      },
-      {
-        name: `チャーシューのタレは大きな鍋に醤油、みりん、清酒、ネギの青い部分、にんにくを潰したもの、生姜薄切りを入れ沸騰させておく`,
-        recipeId: recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
-        index: 2,
-      },
-      {
-        name: `2の肉が柔らかく茹で上がったら
+        "生姜はスライス、長ネギの青い部分と玉ねぎを半分に切り鍋に入れ沸騰したらアクを取り出し弱火で1時間は煮込みます。",
+        `チャーシューのタレは大きな鍋に醤油、みりん、清酒、ネギの青い部分、にんにくを潰したもの、生姜薄切りを入れ沸騰させておく`,
+        `2の肉が柔らかく茹で上がったら
         肉だけ3のタレの鍋に入れ落とし蓋をし1時間煮込み、その後は火を止め放置する`,
-        recipeId: recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
-        index: 3,
-      },
-      {
-        name: "薬味でみじん切りの玉ねぎを用意しチャーシューを切り分けお皿に盛り付け薬味と粗挽き黒胡椒をふり、タレをかければ完成！",
-        recipeId: recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
-        index: 4,
-      },
-      {
-        name: "鶏むね肉を1cm幅にカットする",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 0,
-      },
-      {
-        name: "ビニールに入れて調味料をまぜ、しっかりともみ込む",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 1,
-      },
-      {
-        name: "じゃがいもをみじん切りにして、水でアクを抜く",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 2,
-      },
-      {
-        name: "水をしっかりとり、片栗粉、パン粉と混ぜる",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 3,
-      },
-      {
-        name: "鶏肉に衣をつける",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 4,
-      },
-      {
-        name: "170度の油で揚げる",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 5,
-      },
-      {
-        name: "きつね色になったら、お皿に取り上げてパセリを振りかけて完成！",
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        index: 6,
-      },
-      {
-        name: `材料と調味料を入れて待っだけ
-        通常の炊飯モードで大丈夫です！`,
-        recipeId: recipesNameRecipeIdMap.get("炊飯器で一発OK焼肉屋のビビンパ"),
-        index: 0,
-      },
-      {
-        name: "調味料一式を全てお鍋に入れ沸騰させる",
-        recipeId: recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
-        index: 0,
-      },
-      {
-        name: "鳥もも肉1枚を片目10分強火で煮込む",
-        recipeId: recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
-        index: 1,
-      },
-      {
-        name: "反対側も10分強火で煮込む",
-        recipeId: recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
-        index: 2,
-      },
-      {
-        name: "冷まして味を馴染ませ完成です",
-        recipeId: recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
-        index: 3,
-      },
-      {
-        name: `長ねぎ80g、塩小さじ1/2、味の素8振り、
+        "薬味でみじん切りの玉ねぎを用意しチャーシューを切り分けお皿に盛り付け薬味と粗挽き黒胡椒をふり、タレをかければ完成！",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
+      [
+        "鶏むね肉を1cm幅にカットする",
+        "ビニールに入れて調味料をまぜ、しっかりともみ込む",
+        "じゃがいもをみじん切りにして、水でアクを抜く",
+        "水をしっかりとり、片栗粉、パン粉と混ぜる",
+        "鶏肉に衣をつける",
+        "170度の油で揚げる",
+        "きつね色になったら、お皿に取り上げてパセリを振りかけて完成！",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
+      [
+        "調味料一式を全てお鍋に入れ沸騰させる",
+        "鳥もも肉1枚を片目10分強火で煮込む",
+        "反対側も10分強火で煮込む",
+        "冷まして味を馴染ませ完成です",
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
+      [
+        `長ねぎ80g、塩小さじ1/2、味の素8振り、
         オイスターソース小さじ1、レモン汁小さじ1、
         ごま油大さじ1半、砂糖小さじ1/2、
         黒胡椒を混ぜておく`,
-        recipeId: recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
-        index: 0,
-      },
-      {
-        name: `鶏むね肉350gは塩コショウ、酒大さじ1、
+        `鶏むね肉350gは塩コショウ、酒大さじ1、
         片栗粉小さじ4をもみこむ`,
-        recipeId: recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
-        index: 1,
-      },
-      {
-        name: `さっとゆで
+        `さっとゆで
         ざるで水気を切る`,
-        recipeId: recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
-        index: 2,
-      },
-      {
-        name: "3に1のネギたれをからめて完成",
-        recipeId: recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
-        index: 3,
-      },
-    ] satisfies (Omit<Insertable<RecipeCookingProcedure>, "recipeId"> &
-      Partial<Pick<Insertable<RecipeCookingProcedure>, "recipeId">>)[]
-  ).flatMap((recipeFavorite) => {
-    const { recipeId } = recipeFavorite;
+        "3に1のネギたれをからめて完成",
+      ],
+    ],
+  ] as const satisfies ReadonlyArray<
+    Readonly<
+      [
+        Insertable<RecipeCookingProcedure>["recipeId"] | undefined,
+        ReadonlyArray<Insertable<RecipeCookingProcedure>["name"]>,
+      ]
+    >
+  >;
+
+  const recipeCookingProcedures = recipeCookingProceduresArray.flatMap(([recipeId, cookingProcedureNames]) => {
     if (!recipeId) return [];
-    return [{ ...recipeFavorite, recipeId }];
+    return cookingProcedureNames.map(
+      (cookingProcedureName, index) => ({
+        recipeId,
+        name: cookingProcedureName,
+        index,
+      }),
+      recipeId,
+    );
   });
 
   await db.insertInto("RecipeCookingProcedure").values(recipeCookingProcedures).execute();
--- src/lib/seed/recipe-link-seed.ts
+++ src/lib/seed/recipe-link-seed.ts
@@ -18,80 +18,87 @@ export const recipeLinkSeed = async () => {
     }),
   );
 
-  const recipeLinks = (
+  const recipeLinksArray = [
     [
-      {
-        recipeId: recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
-        url: "https://cookpad.com/recipe/4748850",
-        category: "other",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("最高の明太子パスタ"),
-        url: "https://www.kurashiru.com/recipes/9f751232-2656-43dd-8f31-80fc365ff2b6",
-        category: "other",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
-        url: "https://www.kurashiru.com/recipes/b9c05771-ef26-4c88-8c6f-7692ce1b631e",
-        category: "other",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
-        url: "https://cookpad.com/recipe/1519259",
-        category: "other",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
-        url: "https://cookpad.com/recipe/6652425",
-        category: "other",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"),
-        url: "https://cookpad.com/recipe/1068543",
-        category: "other",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
-        url: "https://www.instagram.com/p/CwpMsJxLzCH/?hl=ja",
-        category: "instagram",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("炊飯器で一発OK焼肉屋のビビンパ"),
-        url: "https://www.instagram.com/p/CwhhTIWuP7B/?hl=ja",
-        category: "instagram",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
-        url: "https://www.instagram.com/p/Cp4SFnYLpc8/?img_index=1",
-        category: "instagram",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
-        url: "https://www.youtube.com/watch?v=b_QxpTyqv2E&ab_channel=%E6%96%99%E7%90%86%E7%A0%94%E7%A9%B6%E5%AE%B6%E3%83%AA%E3%83%A5%E3%82%A6%E3%82%B8%E3%81%AE%E3%83%90%E3%82%BA%E3%83%AC%E3%82%B7%E3%83%94",
-        category: "youtube",
-        index: 0,
-      },
-      {
-        recipeId: recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
-        url: "https://bazurecipe.com/2023/08/17/%e8%b6%85%e7%b5%b6%e6%9f%94%e3%82%89%e3%81%8b%e3%83%8d%e3%82%ae%e5%a1%a9%e3%83%ac%e3%83%a2%e3%83%b3/",
-        category: "other",
-        index: 1,
-      },
-    ] satisfies (Omit<Insertable<RecipeLink>, "recipeId"> & Partial<Pick<Insertable<RecipeLink>, "recipeId">>)[]
-  ).flatMap((recipeLink) => {
-    const { recipeId } = recipeLink;
-    if (!recipeId) return [];
-    return [{ ...recipeLink, recipeId }];
-  });
+      recipesNameRecipeIdMap.get("簡単おいしい♪基本のハンバーグ"),
+      [
+        ["other", "https://cookpad.com/recipe/4748850"],
+        ["instagram", "https://www.instagram.com/"], //dummy data
+        ["facebook", "https://www.facebook.com/"], //dummy data
+        ["tiktok", "https://www.tiktok.com/ja-JP/"], //dummy data
+        ["twitter", "https://twitter.com/home"], //dummy data
+        ["youtube", "https://www.youtube.com/?themeRefresh=1"], //dummy data
+      ],
+    ],
+    [
+      recipesNameRecipeIdMap.get("最高の明太子パスタ"),
+      [["other", "https://www.kurashiru.com/recipes/9f751232-2656-43dd-8f31-80fc365ff2b6"]],
+    ],
+    [
+      recipesNameRecipeIdMap.get("無限パスタ 塩こぶバターときのこ"),
+      [["other", "https://www.kurashiru.com/recipes/b9c05771-ef26-4c88-8c6f-7692ce1b631e"]],
+    ],
+    [
+      recipesNameRecipeIdMap.get("家の黄金比率で♥煮物の定番！肉じゃが♥"),
+      [["other", "https://cookpad.com/recipe/1519259"]],
+    ],
+    [
+      recipesNameRecipeIdMap.get("♡抹茶クリーム大福♡バレンタインにも♡"),
+      [["other", "https://cookpad.com/recipe/6652425"]],
+    ],
+    [recipesNameRecipeIdMap.get("柔ら〜か♪煮豚☆チャーシュー"), [["other", "https://cookpad.com/recipe/1068543"]]],
+    [
+      recipesNameRecipeIdMap.get("お酒飲み専用の無限クランキーチキン"),
+      [["instagram", "https://www.instagram.com/p/CwpMsJxLzCH/?hl=ja"]],
+    ],
+    [
+      recipesNameRecipeIdMap.get("炊飯器で一発OK焼肉屋のビビンパ"),
+      [["instagram", "https://www.instagram.com/p/CwhhTIWuP7B/?hl=ja"]],
+    ],
+    [
+      recipesNameRecipeIdMap.get("辛鶏チャーシュー"),
+      [["instagram", "https://www.instagram.com/p/Cp4SFnYLpc8/?img_index=1"]],
+    ],
+    [
+      recipesNameRecipeIdMap.get("超絶柔らかネギ塩レモン"),
+      [
+        [
+          "youtube",
+          "https://www.youtube.com/watch?v=b_QxpTyqv2E&ab_channel=%E6%96%99%E7%90%86%E7%A0%94%E7%A9%B6%E5%AE%B6%E3%83%AA%E3%83%A5%E3%82%A6%E3%82%B8%E3%81%AE%E3%83%90%E3%82%BA%E3%83%AC%E3%82%B7%E3%83%94",
+        ],
+        [
+          "other",
+          "https://bazurecipe.com/2023/08/17/%e8%b6%85%e7%b5%b6%e6%9f%94%e3%82%89%e3%81%8b%e3%83%8d%e3%82%ae%e5%a1%a9%e3%83%ac%e3%83%a2%e3%83%b3/",
+        ],
+      ],
+    ],
+  ] as const satisfies ReadonlyArray<
+    Readonly<
+      [
+        Insertable<RecipeLink>["recipeId"] | undefined,
+        ReadonlyArray<Readonly<[Insertable<RecipeLink>["category"], Insertable<RecipeLink>["url"]]>>,
+      ]
+    >
+  >;
+
+  const recipeLinks = recipeLinksArray
+    .flatMap(([recipeId, recipeLinks]) => {
+      if (!recipeId) return [];
+      return recipeLinks.map(
+        (recipeLink, index) => ({
+          recipeId,
+          category: recipeLink[0],
+          url: recipeLink[1],
+          index,
+        }),
+        recipeId,
+      );
+    })
+    .flatMap((recipeLink) => {
+      const { recipeId } = recipeLink;
+      if (!recipeId) return [];
+      return [{ ...recipeLink, recipeId }];
+    });
 
   await db.insertInto("RecipeLink").values(recipeLinks).execute();
   console.log("recipeLinkSeed end");
