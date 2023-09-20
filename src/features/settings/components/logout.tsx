81fccc581889c5547bc02825c27bec205a7d2051
n-tsukiya <tkmaru555@gmail.com>
2023-09-20 10:04:30 +0900

    🚧 ログアウト時、TOPページに遷移させたい

--- src/features/settings/components/logout.tsx
+++ src/features/settings/components/logout.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import { signOut } from "next-auth/react";
 import { TbLogout } from "react-icons/tb";
 
 export function Logout() {
+  const router = useRouter();
+
   return (
     <button
       className="flex justify-between px-4 py-3"
       onClick={() => {
         signOut();
+        // TODO: 上手く動かないので解消する
+        router.push("/");
       }}
     >
       <span>ログアウト</span>
