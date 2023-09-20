90e9c4c19a0c1ee7e6e4f9af36db4204aeebcb10
kentakimura <k.kimura0603@gmail.com>
2023-09-20 17:59:39 +0900

    🔥 間違えて含んだ不要ファイル削除

--- src/lib/kysely_org.ts
+++
@@ -1,6 +0,0 @@
-import { Codegen, KyselyAuth } from "@auth/kysely-adapter";
-
-import { dialect } from "@/lib/kysely-node";
-import { DB } from "@/types/db";
-
-export const db = new KyselyAuth<DB, Codegen>({ dialect });
