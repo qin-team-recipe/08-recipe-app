7cf936e015631eb7a5af1f365388fe79527e4586
kentakimura <k.kimura0603@gmail.com>
2023-09-29 15:46:59 +0900

    🐛 型エラー修正、ビルド時こけるため

--- src/app/chef/[id]/popular/page.tsx
+++ src/app/chef/[id]/popular/page.tsx
@@ -1,18 +1,23 @@
-import { VerticalRecipeList } from "@/features/recipes";
+import { randomUUID } from "crypto";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const recipeList = Array.from({ length: 10 }).map(
-    (_, i) =>
-      ({
-        id: i,
-        href: `/recipe`,
-        image: "/images/recipe_02.png",
-        name: "自家燻製したノルウェーサーモンと帆立貝柱のムースのキャベツ包み蒸し 生雲丹とパセリのヴルーテ",
-        chefName: "中々田中ジェフシェフの超々最長ミシシッピレシピ収集",
-        favoriteCount: 1234,
-        isPublic: true,
-      }) as const,
-  );
+import { RecipeListItemType, VerticalRecipeList } from "@/features/recipes";
+
+export default function Page() {
+  const recipeList: RecipeListItemType[] = Array.from({ length: 10 }).map(() => {
+    return {
+      id: randomUUID(),
+      userId: randomUUID(),
+      imgSrc: "/recipe_01.png",
+      name: "自家燻製したノルウェーサーモンと帆立貝柱のムースのキャベツ包み蒸し 生雲丹とパセリのヴルーテ",
+      description: "中々田中ジェフシェフの超々最長ミシシッピレシピ収集",
+      favoriteCount: 1234,
+      servings: 4,
+      isPublic: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  });
+
   return (
     <div className="px-4">
       <VerticalRecipeList recipeList={recipeList} />
