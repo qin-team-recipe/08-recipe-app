750ccb77ad4d92cef304dfd37b1d3595d1d49fbf
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-09-30 22:19:42 +0900

    nameに150文字以内の上限追加

--- src/app/settings/profile/schema.ts
+++ src/app/settings/profile/schema.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const schema = z.object({
-  name: z.string().min(1, { message: "この項目は必須です" }),
+  name: z.string().min(1, { message: "この項目は必須です" }).max(150, { message: "150文字以内で入力してください" }),
   profileImg: z.custom<File>().optional(),
   profileText: z.string().max(200, { message: "200文字以内で入力してください" }).optional(),
   urls: z.array(z.object({ value: z.string().optional() })).refine((urls) => {
