9a24c1b2f98a89ad0fd9124739cda12c9f9dc9d9
yasufumimyoi <yasufumi.myoi@gmail.com>
2023-09-30 16:42:19 +0900

    kyselyの設定を元に戻す

--- src/lib/kysely.ts
+++ src/lib/kysely.ts
@@ -1,20 +1,6 @@
 import { Codegen, KyselyAuth } from "@auth/kysely-adapter";
-import { LogEvent } from "kysely";
 
 import { dialect } from "@/lib/kysely-node";
 import { DB } from "@/types/db";
 
-const kyselyConfig = {
-  dialect,
-  ...(process.env.NODE_ENV === "development" && {
-    log: (event: LogEvent) => {
-      if (event.level == "query") {
-        const q = event.query;
-        const time = Math.round(event.queryDurationMillis * 100) / 100;
-        console.log(`\u001b[32mkysely:sql\u001b[0m [${q.sql}] parameters: [${q.parameters}] time: ${time}`);
-      }
-    },
-  }),
-};
-
-export const db = new KyselyAuth<DB, Codegen>(kyselyConfig);
+export const db = new KyselyAuth<DB, Codegen>({ dialect });
