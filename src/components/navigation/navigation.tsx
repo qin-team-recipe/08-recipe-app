c72b56f1f2a80164d739e98d5917e40725141cbe
yasufumimyoi <yasufumi.myoi@gmail.com>
2023-09-30 18:10:51 +0900

    ロゴの追加

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,14 +1,17 @@
 import { ReactNode } from "react";
 import { Route } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 import { AiOutlineHeart, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 
 export const Navigation = () => {
   return (
-    <nav className="fixed bottom-0 z-50 w-full bg-mauve-1 sm:sticky sm:w-auto sm:self-start">
-      <div className="hidden sm:block  sm:px-3 sm:py-2">ロゴ</div>
-      <div className="flex cursor-pointer gap-1 border-t sm:flex-col sm:border-none">
+    <nav className="fixed bottom-0 z-50 w-full bg-mauve-1 sm:sticky sm:w-auto sm:self-start sm:pr-5 sm:pt-5">
+      <div className="hidden sm:mb-8 sm:block">
+        <Image src="/images/logo.png" width={200} height={37} alt="一流レシピ" />
+      </div>
+      <div className="flex cursor-pointer gap-1 border-t border-mauve-6 sm:flex-col sm:border-none">
         <NavigationItem label="さがす" icon={<AiOutlineSearch />} href={"/"} />
         <NavigationItem label="お気に入り" icon={<AiOutlineHeart />} href={"/fav"} />
         <NavigationItem label="お買い物" icon={<AiOutlineShoppingCart />} href={"/list"} />
@@ -26,10 +29,10 @@ const NavigationItem = <T extends string>({ label, icon, href }: NavigationItemP
   return (
     <Link
       href={href}
-      className="flex w-full flex-col items-center gap-2 py-1 transition-colors duration-200 hover:bg-mauve-4 sm:w-fit sm:flex-row sm:rounded-full sm:px-3 sm:py-2"
+      className="flex w-full flex-col items-center gap-2 py-1 transition-colors duration-200 sm:w-fit sm:flex-row sm:rounded-full sm:px-3 sm:py-2 sm:hover:bg-mauve-4"
     >
-      <div className="text-2xl">{icon}</div>
-      <p className="text-xs sm:text-base">{label}</p>
+      <div className="text-2xl text-mauve-11 sm:text-mauve-12">{icon}</div>
+      <p className="text-xs text-mauve-11 sm:text-base sm:text-mauve-12">{label}</p>
     </Link>
   );
 };
