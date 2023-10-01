994976e838952041fbd828e37a73d5df11e44857
takeyu1013 <yuto1013@gmail.com>
2023-10-01 16:58:42 +0900

    💄 ボーダー色をmauveに変更

--- src/features/recipes/components/recipe-list/recipe-draft-list.tsx
+++ src/features/recipes/components/recipe-list/recipe-draft-list.tsx
@@ -38,7 +38,7 @@ export function RecipeDraftList({ recipe, index }: RecipeDraftList) {
   return (
     <div
       className={cn(
-        "flex h-12 justify-between border-b bg-whitea-13 px-4 py-2 hover:bg-tomato-3",
+        "border-mauve-normal flex h-12 justify-between border-b bg-whitea-13 px-4 py-2 hover:bg-tomato-3",
         index === 0 && "border-t",
       )}
     >
